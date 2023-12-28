401969e48cad6a13e6064596d2c32f6b0a6490c9
suyeong2785 <qkdtpwls_@naver.com>
2023-12-28 17:34:20 +0900

    23/12/28 방수영 1.데이터 삭제기능 구현

--- app/src/main/java/com/example/chapter4/MainActivity.kt
+++ app/src/main/java/com/example/chapter4/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.core.view.isVisible
 import com.example.chapter4.databinding.ActivityMainBinding
 
@@ -18,6 +19,10 @@ class MainActivity : AppCompatActivity() {
         binding.goInputActivityButton.setOnClickListener {
             val intent = Intent(this,EditActivity::class.java)
             startActivity(intent)
+        }
+
+        binding.deleteButton.setOnClickListener {
+            deleteData()
         }
     }
 
@@ -41,5 +46,15 @@ class MainActivity : AppCompatActivity() {
                 binding.warningValueTextView.text = warning
             }
         }
+    }
+
+    private fun deleteData() {
+        with(getSharedPreferences(USER_INFORMATION, MODE_PRIVATE).edit()) {
+            clear()
+            apply()
+        }
+        getDataAndUiUpdate()
+
+        Toast.makeText(this, "초기화를 완료하였습니다.", Toast.LENGTH_SHORT).show()
     }
 }
